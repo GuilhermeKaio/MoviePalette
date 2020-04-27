2e5ff4e47ddefa8648c75dce3693597344767e71
Guilherme Kaio <31046437+GuilhermeKaio@users.noreply.github.com>
2020-04-27 17:50:29 -0300

    Hero erro

--- index.js
+++ index.js
@@ -1,18 +1,12 @@
 const imdbMovieScrap = require('./scripts/imdbmoviescrap.js')
-const imdbTVScrap = require('./scripts/imdbtvscrap.js')
+//const imdbTVScrap = require('./scripts/imdbtvscrap.js')
 const info = require('./scripts/info.js')
 const image = require('./scripts/image.js')
 const twitter = require('./scripts/twitter.js')
 const uniqueRandom = require('unique-random')
 
 async function start() {
-    const random = uniqueRandom(0, 1)
-    if (random() == 0){
-        List = await imdbMovieScrap()
-    }
-    else{
-        List = await imdbTVScrap()
-    }
+    List = await imdbMovieScrap()
     console.log(List)
     const randomList = uniqueRandom(0, List.length)
     infoList = await info(List[randomList()])
